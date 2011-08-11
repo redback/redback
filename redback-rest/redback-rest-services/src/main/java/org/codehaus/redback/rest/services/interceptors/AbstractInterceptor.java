5ee8b19383e479799f2fab058ede460cb7318957
Olivier Lamy <olamy@apache.org>
2011-08-11 10:37:55 +0000

    add missing license header

--- redback-rest/redback-rest-services/src/main/java/org/codehaus/redback/rest/services/interceptors/AbstractInterceptor.java
+++ redback-rest/redback-rest-services/src/main/java/org/codehaus/redback/rest/services/interceptors/AbstractInterceptor.java
@@ -1,5 +1,21 @@
 package org.codehaus.redback.rest.services.interceptors;
 
+/*
+* Copyright 2011 The Codehaus.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 import org.apache.cxf.message.Message;
 import org.codehaus.plexus.redback.authorization.RedbackAuthorization;
 import org.slf4j.Logger;
@@ -10,11 +26,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
 
 /**
- * Created by IntelliJ IDEA.
- * User: olamy
- * Date: 11/08/11
- * Time: 12:09
- * To change this template use File | Settings | File Templates.
+ * @since 1.3
+ * @author Olivier Lamy
  */
 public class AbstractInterceptor
 {
